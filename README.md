05906f10980949944dd52a0a29c0ec775770ac80
s1963887 <s1963887@online.houstonisd.org>
2021-03-29 11:57:09 -0500

    creating a form

---
+++ README.md
@@ -0,0 +1,5 @@
+# Car Racing Stage 0.5
+● Create a form to log the players' name in the game.
+● Update playerCount and gameState in the database.
+● Use OOPs programming style.
+
---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas;
+var database;
+var gameState = 0;
+var playerCount;
+var form, game, player;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+
+
+function draw(){
+
+}
